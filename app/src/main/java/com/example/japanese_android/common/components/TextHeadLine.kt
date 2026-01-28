349f8e9ec13df290930c5dce9dd074b47e84dc6a
Orion <nnmm5122004@gmail.com>
2026-01-28 15:04:00 +0630

    :sparkels: home page

---
+++ app/src/main/java/com/example/japanese_android/common/components/TextHeadLine.kt
@@ -0,0 +1,19 @@
+package com.example.japanese_android.common.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun TextHeadLine(
+    text : String,
+    style: TextStyle = MaterialTheme.typography.headlineSmall
+){
+    Text(
+        text,
+        style = style,
+        fontWeight = FontWeight.Bold
+    )
+}
---
+++ app/src/main/java/com/example/japanese_android/features/home/presentation/components/LearningModuleCard.kt
@@ -0,0 +1,102 @@
+package com.example.japanese_android.features.home.presentation.components
+
+import android.text.Layout
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.japanese_android.common.components.TextTitle
+
+@Composable
+fun LearningModuleCard(
+    title: String,
+    desc: String,
+    iconText: String,
+    iconColor: Color,
+    onClick: () -> Unit
+) {
+    ElevatedCard(
+        onClick = onClick,
+        modifier = Modifier
+            .height(200.dp)
+            .padding(vertical = 5.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 1.dp
+        ),
+    ) {
+        Column(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            // UPPER SECTION
+            Row(
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                // LEFT SECTION
+                Column() {
+                    TextTitle(title = title)
+                    Text(
+                        desc.trimIndent(),
+                        modifier = Modifier.padding(vertical = 15.dp),
+                        fontSize = 16.sp
+                    )
+                }
+                Spacer(Modifier.weight(1f))
+                //   RIGHT SECTION
+                Box(
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(CircleShape)
+                        .background(iconColor),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(iconText, fontSize = 45.sp)
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            // LOWER SECTION
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Start learning", fontSize = 13.sp)
+                IconButton(onClick = onClick) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowRight,
+                        contentDescription = "KeyboardArrowRight"
+                    )
+                }
+            }
+        }
+    }
+}
