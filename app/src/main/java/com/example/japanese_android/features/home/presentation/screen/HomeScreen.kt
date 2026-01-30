38d61b74540ebdcaae59baee8cbcc40f45c131ae
Orion <nnmm5122004@gmail.com>
2026-01-30 08:46:02 +0630

    :recycle: seperate component

--- app/src/main/java/com/example/japanese_android/features/home/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/example/japanese_android/features/home/presentation/screen/HomeScreen.kt
@@ -35,6 +35,7 @@ import com.example.japanese_android.common.components.BottomNavBar
 import com.example.japanese_android.common.components.TextHeadLine
 import com.example.japanese_android.features.home.data.model.HomePageModules
 import com.example.japanese_android.features.home.presentation.components.FloatingTopBar
+import com.example.japanese_android.features.home.presentation.components.HomeContent
 import com.example.japanese_android.features.home.presentation.components.LearningModuleCard
 import com.example.japanese_android.features.home.utils.CircleClip
 import com.example.japanese_android.ui.state.calculateScrollDirection
@@ -139,125 +140,6 @@ fun HomeScreen(
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun HomeContent(
-    scrollState: LazyListState,
-    isScrollUp: Boolean,
-    isDark: Boolean,
-    entryVisibleState: MutableTransitionState<Boolean>,
-    iconDarkPreview: Boolean,
-    onToggleRequest: (Offset) -> Unit,
-    onEntryShown: () -> Unit,
-    isAnimatingTheme: Boolean
-) {
-    val textColor = MaterialTheme.colorScheme.onBackground
-    LaunchedEffect(entryVisibleState.currentState) {
-        if (entryVisibleState.currentState) {
-            onEntryShown()
-        }
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .statusBarsPadding()
-    ) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            FloatingTopBar(
-                title = "MUDA-ZERO",
-                isDarkTheme = iconDarkPreview,
-                animateIcon = !isAnimatingTheme,
-                isToggleEnabled = !isAnimatingTheme,
-                onThemeToggle = {
-                    offset -> onToggleRequest(offset)
-                }
-            )
-
-            LazyColumn(
-                state = scrollState,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 20.dp)
-            ) {
-                item {
-                    // Only apply slide-in to the header on first load
-                    AnimatedVisibility(
-//                        visibleState = entryVisibleState,
-                        visible = entryVisibleState.targetState && !isAnimatingTheme,
-                        enter = fadeIn(tween(500)) + slideInVertically(initialOffsetY = { 40 }),
-                        exit = fadeOut()
-                    ) {
-                        Column {
-                            Spacer(modifier = Modifier.height(30.dp))
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                TextHeadLine("Welcome to ", color = textColor)
-                                AppLogo()
-                                Spacer(modifier = Modifier.weight(.1f))
-                                AppName(style = MaterialTheme.typography.headlineSmall, color = textColor)
-                                Spacer(modifier = Modifier.weight(1f))
-                            }
-                            Spacer(modifier = Modifier.height(10.dp))
-                            Text("Ready to Start your journey? Pick a module below to master your fluency.", color = textColor)
-                            Spacer(modifier = Modifier.height(40.dp))
-                        }
-                    }
-                }
-
-                item {
-                    TextHeadLine("Learning Modules", color = textColor)
-                    Spacer(modifier = Modifier.height(10.dp))
-                }
-
-                itemsIndexed(
-                    items = HomePageModules,
-                    key = { _, module -> module.title } // Keeps scroll state synced
-                ) { index, module ->
-
-                    // IF we are animating the theme reveal, DO NOT run the scale animation
-                    // This prevents the cards from "locking" or flickering
-                    if (isAnimatingTheme) {
-                        LearningModuleCard(
-                            title = module.title,
-                            desc = module.desc,
-                            iconText = module.icon,
-                            iconColor = module.color,
-                            onClick = {}
-                        )
-                    } else {
-                        // This only runs on first app entry
-                        val itemVisible = remember { MutableTransitionState(false).apply { targetState = true } }
-                        AnimatedVisibility(
-                            visibleState = itemVisible,
-                            enter = fadeIn(tween(800, delayMillis = 120 * index)) +
-                                    scaleIn(initialScale = 0.95f, animationSpec = tween(800, delayMillis = 140 * index))
-                        ) {
-                            LearningModuleCard(
-                                title = module.title,
-                                desc = module.desc,
-                                iconText = module.icon,
-                                iconColor = module.color,
-                                onClick = {}
-                            )
-                        }
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-                item { Spacer(modifier = Modifier.height(100.dp)) }
-            }
-        }
-
-        // Bottom Navbar
-        AnimatedVisibility(
-            visible = (isScrollUp || scrollState.firstVisibleItemScrollOffset == 0) && !isAnimatingTheme,
-            enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
-            exit = slideOutVertically(targetOffsetY = { it }) + fadeOut(),
-            modifier = Modifier.align(Alignment.BottomCenter)
-        ) {
-            BottomNavBar()
         }
     }
 }
