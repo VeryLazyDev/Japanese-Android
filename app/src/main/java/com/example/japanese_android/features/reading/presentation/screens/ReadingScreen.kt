b716062c35497047d9256293a955dde684b3ecf8
Orion <nnmm5122004@gmail.com>
2026-01-30 16:28:59 +0630

    :sparkles: add bottom nav in reading page

--- app/src/main/java/com/example/japanese_android/features/reading/presentation/screens/ReadingScreen.kt
+++ app/src/main/java/com/example/japanese_android/features/reading/presentation/screens/ReadingScreen.kt
@@ -5,11 +5,17 @@ import androidx.compose.animation.core.EaseInOut
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
@@ -18,30 +24,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.japanese_android.common.components.TextHeadLine
+import com.example.japanese_android.common.components.bottom_nav.BottomNavBar
 import kotlinx.coroutines.delay
 
 @Composable
 fun ReadingScreen(
     navController: NavController
 ) {
-    var isTitleVisible by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        delay(700)
-        isTitleVisible = true
-    }
     Box(
         modifier = Modifier
             .statusBarsPadding()
-            .safeContentPadding()
+//            .safeContentPadding()
             .fillMaxSize()
     ) {
-        Column() {
-//            if (isTitleVisible) {
-//            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 15.dp)
+        ) {
             val itemVisible =
                 remember { MutableTransitionState(false).apply { targetState = true } }
             AnimatedVisibility(
@@ -51,10 +56,23 @@ fun ReadingScreen(
                             initialScale = 0.8f,
                             animationSpec = tween(1500)
                         )
-            ){
+            ) {
                 TextHeadLine("Learning Modules")
-
             }
+
+        Spacer(modifier = Modifier.weight(1f))
+        }
+        AnimatedVisibility(
+            visible = true,
+            enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
+            exit = slideOutVertically(targetOffsetY = { it }) + fadeOut(),
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+        ) {
+            BottomNavBar(
+                navController = navController
+            )
         }
     }
 }
